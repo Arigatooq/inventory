0652acd1d9930e6678de5032a2a89ae99e78b861
Arigatooq <McKrosh@mail.ru>
2024-07-15 07:36:02 +0500

    added InfiniteSkeleton

--- app.vue
+++ app.vue
@@ -14,7 +14,7 @@ const modalInfo = ref(
     col: null,
     content: null 
   }
-);
+)
 
 grid.value[0][0] = qwe
 grid.value[0][1] = asd
@@ -23,30 +23,33 @@ grid.value[0][1] = asd
 let dragSource = { 
   row: null,
   col: null 
-};
+}
 
 const dragStart = (event, rowIndex, colIndex) => {
-  dragSource = { row: rowIndex, col: colIndex };
-  event.dataTransfer.effectAllowed = 'move';
-};
+  dragSource = { 
+     row: rowIndex,
+     col: colIndex 
+    }
+  event.dataTransfer.effectAllowed = 'move'
+}
 
 
 const dragEnter = (event, rowIndex, colIndex) => {
-  event.target.classList.add('drag-over');
+  event.target.classList.add('drag-over')
 };
 
 const dragLeave = (event, rowIndex, colIndex) => {
-  event.target.classList.remove('drag-over');
+  event.target.classList.remove('drag-over')
 };
 
 const drop = (event, rowIndex, colIndex) => {
-  event.target.classList.remove('drag-over');
-  const targetCell = grid.value[rowIndex][colIndex];
-  const sourceCell = grid.value[dragSource.row][dragSource.col];
+  event.target.classList.remove('drag-over')
+  const targetCell = grid.value[rowIndex][colIndex]
+  const sourceCell = grid.value[dragSource.row][dragSource.col]
 
   if (targetCell === null) {
-    grid.value[rowIndex][colIndex] = sourceCell;
-    grid.value[dragSource.row][dragSource.col] = null;
+    grid.value[rowIndex][colIndex] = sourceCell
+    grid.value[dragSource.row][dragSource.col] = null
   }
 };
  const showCell = (rowIndex, colIndex, cell) =>{
@@ -57,16 +60,25 @@ const drop = (event, rowIndex, colIndex) => {
       row: rowIndex,
       col: colIndex,
       content: cell
-    };
-    showModal.value = true;
+    }
+    showModal.value = true
   }
  }
  const closeModal = () => {
-  showModal.value = false;
-};
+  showModal.value = false
+}
+const getCellClass = (rowIndex, colIndex) => {
+  const classes = [];
+  if (rowIndex === 0 && colIndex === 0) classes.push('top-left');
+  if (rowIndex === 0 && colIndex === 4) classes.push('top-right');
+  if (rowIndex === 4 && colIndex === 0) classes.push('bottom-left');
+  if (rowIndex === 4 && colIndex === 4) classes.push('bottom-right');
+  return classes.join(' ')
+}
 </script>
 
 <template>
+  <div class="main">
   <div class="flex-container">
     <div>
     <div v-for="(row, rowIndex) in grid" :key="rowIndex" class="grid-row">
@@ -74,11 +86,13 @@ const drop = (event, rowIndex, colIndex) => {
         v-for="(cell, colIndex) in row"
         :key="colIndex"
         class="grid-cell"
+        :class="getCellClass(rowIndex, colIndex)"
         @dragover.prevent
         @dragenter="dragEnter($event, rowIndex, colIndex)"
         @dragleave="dragLeave($event, rowIndex, colIndex)"
         @drop="drop($event, rowIndex, colIndex)"
         @click="showCell(rowIndex, colIndex, cell)"
+
       >
         <img
           v-if="cell"
@@ -95,17 +109,33 @@ const drop = (event, rowIndex, colIndex) => {
         <div class="close" @click="closeModal"></div>
         <div class="divImg"><img v-if="modalInfo.content" :src="modalInfo.content" alt="cell image" class="contentImg"/></div>
         <div class="border"></div>
-        <button class="delete">Удалить предмет</button>
+        <div class="btnFlex"><button class="delete">Удалить предмет</button></div>
       </div>
     </div>
   </div>
+  <div>
+    <div class="modal">
+      <div class="left-content">
+        <div class="divImgLeft"><img src="/public/img.png" alt="imageLeft" class="contentImg"/></div>
+        <InfiniteSkeleton/>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <style scoped>
+  .main{
+    background-color: rgba(38, 38, 38, 1);
+    height: 100vh;
+  }
   .flex-container{
     display: flex;
     justify-content: center;
     gap: 40px;
+    height: 100vh;
+    align-items: center;
+    
   }
   .container{
     max-width: 1080px;
@@ -114,7 +144,7 @@ const drop = (event, rowIndex, colIndex) => {
   .grid-cell {
   width: 150px;
   height: 150px;
-  border: 1px solid #000;
+  border: 1px solid rgba(77, 77, 77, 1);
   display: flex;
   align-items: center;
   justify-content: center;
@@ -124,6 +154,7 @@ const drop = (event, rowIndex, colIndex) => {
 .grid-row{
   display: flex;
 }
+
 .contentImg{
   width: 250px;
   height: 250px;
@@ -133,16 +164,20 @@ const drop = (event, rowIndex, colIndex) => {
   justify-content: center;
 }
 .modal-content{
+  top: 106px;
   position: absolute;
   left: 970px;
-  border: 1px solid rgb(48, 47, 47);
+  border: 1px solid rgba(77, 77, 77, 1);
   width: 350px;
   height: 758px;
   background-color: gray;
+  border-radius: 0px 15px 15px 0px;
+  background-color: rgba(38, 38, 38, 1);
+  transform: translateX(-150%);
+  animation: ani 1s forwards;
+
 }
-.close{
-  cursor: pointer;
-}
+
 .border{
   margin: 15px 0px;
   border: 1px solid rgb(201, 198, 198);
@@ -151,23 +186,64 @@ const drop = (event, rowIndex, colIndex) => {
   width: 1em;
   height: 1em;
   background-color: rgb(216, 216, 216);
-  margin-left: auto;
+  margin-top: 10px;
+  margin-left: 320px;
   clip-path: polygon(20% 0%, 0% 20%, 30% 50%, 0% 80%, 20% 100%, 50% 70%, 80% 100%, 100% 80%, 70% 50%, 100% 20%, 80% 0%, 50% 30%);
+  cursor: pointer;
+
 }
 .delete{
-  margin-top: 390px;
-  margin-left: 50px;
-  width: 250px;
+  width: 220px;
   border-radius: 11px;
   border: none;
-  height: 40px;
-  background: rgb(255, 114, 161);
+  height: 39px;
+  background-color: rgba(250, 114, 114, 1);
   color: white;
   font-size: 16px;
   cursor: pointer;
 }
+.btnFlex{
+  display: flex;
+  justify-content: center;
+  align-items: end;
+  height: 420px;
+}
 .delete:hover{
   opacity: 0.5;
 }
+.top-left {
+  border-top-left-radius: 10px;
+}
+.top-right {
+  border-top-right-radius: 10px;
+}
+.bottom-left {
+  border-bottom-left-radius: 10px;
+}
+.bottom-right {
+  border-bottom-right-radius: 10px;
+}
 
+@keyframes ani {
+  0% {transform: translateX(25%);}
+  100% {transform: translateY(0);}
+}
+.left-content{
+  top: 106px;
+  position: absolute;
+  left: 170px;
+  border: 1px solid rgba(77, 77, 77, 1);
+  width: 350px;
+  height: 758px;
+  background-color: gray;
+  border-radius: 15px 15px 15px 15px;
+  background-color: rgba(38, 38, 38, 1);
+}
+.divImgLeft{
+ display: flex;
+ justify-content: center;
+ height: 300px;
+ align-items: center;
+
+}
 </style>
---
+++ components/InfiniteSkeleton.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="main_flex">
+        <div class="skeleton-cell"></div><br>
+        <div class="skeleton-cell-2"></div><br>
+        <div class="skeleton-cell-3"></div><br>
+        <div class="skeleton-cell-4"></div><br>
+        <div class="skeleton-cell-5"></div><br>
+        <div class="skeleton-cell-6"></div><br>
+        <div class="skeleton-cell-7"></div><br>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  
+  const items = ref([]);
+
+  </script>
+  
+  <style scoped>
+  .main_flex{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .skeleton-cell-2{
+    border-radius: 8px;
+    width: 250px;
+    height: 15px;
+    margin-right: 10px;
+    background-color: #ccc;
+    animation: pulse 1.5s infinite;
+    margin-top: 15px;
+  }
+  .skeleton-cell-3{
+    border-radius: 8px;
+    width: 310px;
+    height: 18px;
+    margin-right: 10px;
+    background-color: #ccc;
+    animation: pulse 1.5s infinite;
+  }
+  .skeleton-cell-4{
+    border-radius: 8px;
+    width: 295px;
+    height: 18px;
+    margin-right: 10px;
+    background-color: #ccc;
+    animation: pulse 1.5s infinite;
+  }
+  .skeleton-cell-5{
+    border-radius: 8px;
+    width: 275px;
+    height: 18px;
+    margin-right: 10px;
+    background-color: #ccc;
+    animation: pulse 1.5s infinite;
+  }
+  .skeleton-cell-6{
+    border-radius: 8px;
+    width: 255px;
+    height: 18px;
+    margin-right: 10px;
+    background-color: #ccc;
+    animation: pulse 1.5s infinite;
+  }
+  .skeleton-cell-7 {
+    border-radius: 8px;
+    width: 150px;
+    height: 18px;
+    margin-right: 10px;
+    background-color: #ccc;
+    animation: pulse 1.5s infinite;
+    margin-top: 40px;
+  }
+  .skeleton-cell {
+    border-radius: 8px;
+    width: 300px;
+    height: 30px;
+    margin-right: 10px;
+    background-color: #ccc;
+    animation: pulse 1.5s infinite;
+
+  }
+  @keyframes pulse {
+    0% {
+      background-color: rgba(60, 60, 60, 1);
+    }
+    50% {
+      background-color: rgba(68, 68, 68, 1);
+    }
+    100% {
+      background-color: rgba(51, 51, 51, 1);
+    }
+  }
+  </style>
+  
